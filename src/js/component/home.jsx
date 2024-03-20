8d9c754f45a8c9f95b0948e1101671feb2e2ecee
CesarMendez1 <152318003+CesarMendez1@users.noreply.github.com>
2024-03-20 01:11:24 +0000

    casi listo gracias a astrid y la mentoria. solo falta ponerlo bonito

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+	const imput = (e) => {
+		let texto = e.target.value;
+		if (e.keyCode == 13) {
+			setTarea(texto)
+			setLista([...lista, texto])
+			setTarea(" ")
+		}
+	}
+	const borrar =(index)=>{
+	const nuevalista=[...lista]	
+	nuevalista.splice(index,1)
+	setLista(nuevalista)
+	}
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="mb-3">
+				<label htmlFor="exampleFormControlInput1" className="form-label">To do list</label>
+				<input type="text"  className="form-control" id="exampleFormControlInput1" placeholder="Agregar tarea"  onKeyUp={(e) => { imput(e) }} />
+			</div>
+			<ol>
+				{lista.map((item, index) => {
+					return (
+						<li key={index}>
+							{item}
+							<button onClick={()=>borrar(index)}>
+								x
+							</button>
+						</li>)
+				})}
+			</ol>
 		</div>
 	);
 };
